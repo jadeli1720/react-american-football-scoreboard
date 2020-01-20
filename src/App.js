648fe7268e8af83364ee87cc746540fa76a59060
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-01-19 17:48:39 -0700

    Made second component and passed props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./Components/BottomRow";
+import TopRow from "./Components/TopRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -9,26 +10,13 @@ function App() {
   let [ homeScore, setHomeScore ] = useState(0);
   let [awayScore, setAwayScore ] = useState(0);
 
-  console.log("homeScore", homeScore)
-  console.log("awayScore", awayScore)
+  // console.log("homeScore", homeScore)
+  // console.log("awayScore", awayScore)
 
   return (
     <div className="container">
       <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score"> {homeScore} </div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score"> {awayScore} </div>
-          </div>
-        </div>
+        <TopRow homeScore={homeScore} awayScore ={awayScore} />
         <BottomRow />
       </section>
       <section className="buttons">
@@ -39,7 +27,7 @@ function App() {
         </div>
         <div className="awayButtons">
           <button onClick={() => setAwayScore( awayScore += 7)} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={() => setAwayScore( awayScore += 0)} className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={() => setAwayScore( awayScore += 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
